0dc969518be1bf7f2f2bc277475a18cea591b6e8
Ali <alikamali0328@gmail.com>
2024-06-05 17:52:40 +0330

    Step 2: Getting data and changing the format on another module

--- js/game.js
+++ js/game.js
@@ -1,1 +1,23 @@
-console.log("game");
+import formatData from "./helper.js";
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData = null;
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  formattedData = formatData(json.results);
+  console.log(formattedData);
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
