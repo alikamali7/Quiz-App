770acb267c7f144ccd7fce663e7c1c4c879c85c7
Ali <alikamali0328@gmail.com>
2024-06-11 12:58:13 +0330

    Step 5: Save the results of the questions and show the high scores and determine the difficulty level of the questions

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 import formatData from "./helper.js";
 
+const level = localStorage.getItem("level") || "medium";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
@@ -8,10 +10,10 @@ const scoreText = document.getElementById("score");
 const nextButton = document.getElementById("next-button");
 const finishButton = document.getElementById("finish-button");
 const questionNumber = document.getElementById("question-number");
+const error = document.getElementById("error");
 
 const CORRECT_BONUS = 10;
-const URL =
-  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+const URL = `https://opentdb.com/api.php?amount=10&difficulty=${level}&type=multiple`;
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
@@ -19,10 +21,16 @@ let score = 0;
 let isAccepted = true;
 
 const fetchData = async () => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  formattedData = formatData(json.results);
-  start();
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    formattedData = formatData(json.results);
+    start();
+  } catch (error) {
+    loader.style.display = "none";
+    error.style.display = "block";
+    
+  }
 };
 
 const start = () => {
@@ -36,7 +44,6 @@ const showQuestion = () => {
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
   correctAnswer = correctAnswerIndex;
-  console.log(correctAnswer);
   questionText.innerText = question;
   answerList.forEach((button, index) => {
     button.innerText = answers[index];
@@ -65,7 +72,7 @@ const nextHandler = () => {
     removeClasses();
     showQuestion();
   } else {
-    finishHandler()
+    finishHandler();
   }
 };
 
@@ -75,8 +82,8 @@ const removeClasses = () => {
 
 const finishHandler = () => {
   localStorage.setItem("score", JSON.stringify(score));
-    window.location.assign("/end.html");
-}
+  window.location.assign("/end.html");
+};
 
 window.addEventListener("load", fetchData);
 nextButton.addEventListener("click", nextHandler);
